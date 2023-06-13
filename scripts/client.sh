2d7d1b8d0509601e1081d33141514c0968d1cdd3
mischmetal <ayumishra.95@gmail.com>
2023-06-13 20:36:46 +0800

    added a new experimental pif logic

--- scripts/client.sh
+++ scripts/client.sh
@@ -7,7 +7,7 @@ sudo ifconfig ingress mtu 100
 #wget -U Mozilla https://www.youtube.com/ -O index
 #wget -U Mozilla https://open.spotify.com/user/deutschegrammophon/playlist/2B11k6zJ2vIJTjOiqz3Y35 -O index
 #wget -U Mozilla https://www.instagram.com/static/bundles/es6/FeedPageContainer.js/434e5de15e7c.js -O index
-#wget -U Mozilla https://www.reddit.com/r/AskReddit/comments/brlti4/reddit_what_are_some_underrated_apps/ -O index
+wget -U Mozilla https://www.reddit.com/r/AskReddit/comments/brlti4/reddit_what_are_some_underrated_apps/ -O index
 
 #wget -U Mozilla hombre.d2.comp.nus.edu.sg:8080/tcpcensus_sigmetrics2020.pdf -O index
 
@@ -19,7 +19,8 @@ name=$1
 cd ..
 cd selenium/chrome
 
-python3 spotify.py $name
+#wget -U Mozilla https://www.zhihu.com/people/jie-zhi-61/creations/19573493 -O index
+#python3 spotify.py $name
 
 cd ..
 cd ..
