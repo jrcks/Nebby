acfd38c5a09d85e2c3f4d7bc5df8cad05698d279
lakshayr31 <62927877+lakshayr31@users.noreply.github.com>
2024-09-07 13:46:16 +0530

    Update _run_custom_server.sh removed password

--- custom_clients/_run_custom_server.sh
+++ custom_clients/_run_custom_server.sh
@@ -1,4 +1,4 @@
 cc=$1
 SCRIPT="./compile_server.sh; ./server 1 index.html ${cc}"
 echo "In the Script to RUN the SERVER"
-sshpass -p 'lakshay123' ssh lakshay@edith.d2.comp.nus.edu.sg "${SCRIPT}"
+sshpass -p {SERVER_PASSWORD] ssh [IP_SERVER] "${SCRIPT}"
