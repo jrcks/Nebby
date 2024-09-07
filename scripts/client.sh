754c3862677f1b9c54936201a26138f74f6f128e
lakshayr31 <62927877+lakshayr31@users.noreply.github.com>
2024-09-07 13:40:48 +0530

    Update client.sh - removed confidential information

--- scripts/client.sh
+++ scripts/client.sh
@@ -15,9 +15,9 @@ link=$2
 
 #sudo echo "0" > /proc/sys/net/ipv4/tcp_sack
 # sudo tcpdump -i ingress -w aft-btl-test.pcap &
-# iperf3 -c 54.254.209.251 -p 2500 -C $cc -t 60 -R --connect-timeout 2000 -M 100
+# iperf3 -c [IP_SERVER} -p 2500 -C $cc -t 60 -R --connect-timeout 2000 -M 100
 #sudo sysctl net.ipv4.tcp_congestion_control=$1
-#iperf -c edith.d2.comp.nus.edu.sg -p 5000 -t 30 -Z $1
+#iperf -c [IP_SERVER] -p 5000 -t 30 -Z $1
 #wget -U Mozilla https://www.youtube.com/ -O index
 #wget -U Mozilla https://open.spotify.com/user/deutschegrammophon/playlist/2B11k6zJ2vIJTjOiqz3Y35 -O index
 #wget -U Mozilla https://www.instagram.com/static/bundles/es6/FeedPageContainer.js/434e5de15e7c.js -O index
