25cab42722137b1f2a26ea3772d5e6657a063617
Julian <jrocks@uos.de>
2025-01-23 22:49:32 +0100

    Cleaned install script and link lists

--- chmod_files.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-files="install_dependencies.sh ./scripts/_test_wget.sh ./scripts/new-btl.sh ./scripts/clean.sh ./scripts/simnet.sh ./scripts/client.sh"
-sudo chmod +x $files
---
+++ install.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+
+# Exit immediately if a command exits with a non-zero status
+set -e
+
+# Install git and mahimahi dependencies
+DEPS="
+    git
+    # === mahimahi dependencies === #
+    protobuf-compiler
+    libprotobuf-dev
+    autotools-dev
+    dh-autoreconf
+    iptables
+    pkg-config
+    dnsmasq-base
+    apache2-bin
+    debhelper
+    libssl-dev
+    ssl-cert
+    libxcb-present-dev
+    libcairo2-dev
+    libpango1.0-dev
+"
+
+# Update package list and install dependencies
+sudo apt update
+sudo apt install -y $DEPS
+
+# Install mahimahi as per http://mahimahi.mit.edu/
+git clone https://github.com/ravinet/mahimahi
+cd mahimahi || { echo "Failed to clone mahimahi."; exit 1; }
+
+# Run the installation steps
+./autogen.sh
+./configure
+make
+sudo make install
+
+# Mark scripts as executable
+FILES="
+    ./scripts/_test_wget.sh
+    ./scripts/new-btl.sh
+    ./scripts/clean.sh
+    ./scripts/simnet.sh
+    ./scripts/client.sh
+"
+sudo chmod +x $FILES
+
+# Echo success message
+echo "Installation completed successfully!"
