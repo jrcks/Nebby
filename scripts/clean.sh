377a503f9fed9089a4dec722b958bd7a38af0b00
Julian <jrocks@uos.de>
2025-02-01 14:09:50 +0100

    Extended clean script to remove output directory and compiled custom clients

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -2,3 +2,9 @@
 
 # Stop all running processes
 sudo killall iperf iperf3 tcpdump mm-delay mm-link
+
+# Remove the output directory
+rm -rf ../measurements
+
+# Remove compiled custom clients
+rm ../custom_clients/client ../custom_clients/server ../custom_clients/sender ../custom_clients/receiver
