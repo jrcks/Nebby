d0612c1b470c84a68019ab129c9425a184f88fce
Rasmus-123 <58940717+Rasmus-123@users.noreply.github.com>
2025-02-04 17:40:18 +0100

    Added check_whole_cc_folder.sh script

---
+++ analysis/check_whole_cc_folder.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+filters=("0-50-200-1 0-50-200-2 0-50-1000-1 0-50-1000-2 0-100-200-1 0-100-200-2 0-100-1000-1 0-100-1000-2")
+
+folder=$1
+echo "Checking folder $folder"
+
+for filter in ${filters[@]}; do
+    echo "Checking filter $filter"
+    python3 final/check_cc_folder.py $folder $filter "$filter.res"
+done
+
+if [ -z "$2" ]; then
+    exit 0
+fi
+
+echo ""
+echo "===================="
+echo ""
+
+for filter in ${filters[@]}; do
+    echo "Filter: $filter"
+    cat "$filter.res"
+done
