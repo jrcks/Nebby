6854ec68f2ad770b242d570fc996c56044bb7445
Julian <jrocks@uos.de>
2025-02-07 01:25:36 +0100

    Improved logging

--- scripts/loadCCAs.sh
+++ scripts/loadCCAs.sh
@@ -12,11 +12,11 @@ echo -e "Currently active congestion control algorithm: ${BLD}$current_cca${RST}
 
 # Display available congestion control algorithms
 available_cca=$(sysctl -n net.ipv4.tcp_available_congestion_control)
-echo -e "Available congestion control algorithms: ${BLD}$available_cca${RST}"
+echo -e "Available congestion control algorithms:\n${BLD}$available_cca${RST}"
 
 # Display currently allowed congestion control algorithms
 allowed_cca=$(sysctl -n net.ipv4.tcp_allowed_congestion_control)
-echo -e "Allowed congestion control algorithms: ${BLD}$allowed_cca${RST}"
+echo -e "Allowed congestion control algorithms:\n${BLD}$allowed_cca${RST}"
 
 # Get list of available congestion control algorithms
 kernel_version=$(uname -r)
@@ -24,7 +24,7 @@ module_cca=$(find /lib/modules/"$kernel_version"/kernel/net/ipv4/ -name 'tcp_*.k
 
 # Display available algorithms
 if [ -n "$module_cca" ]; then
-    echo -e "Found the module of the congestion control algorithms: ${BLD}$module_cca${RST}"
+    echo -e "Found the module of the congestion control algorithms:\n${BLD}$module_cca${RST}"
 else
     echo -e "${RED}No available congestion control algorithms modules found.${RST}" >&2
     exit 1
@@ -34,7 +34,7 @@ fi
 IFS=',' read -r -a algos <<<"$module_cca"
 for cca in "${algos[@]}"; do
     cca=$(echo "$cca" | xargs) # Trim whitespace
-    echo -n "Loading congestion control algorithm: $cca... "
+    echo -n "Loading congestion control algorithm: ${BLD}$cca${RST}... "
     if sudo modprobe "tcp_$cca"; then
         # Add the loaded algorithm to the allowed list by loading it
         sudo sysctl -w net.ipv4.tcp_congestion_control="$cca" 1>/dev/null
