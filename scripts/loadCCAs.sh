94ff791ca0bc0609b99f8293352add31e6a55b5b
Julian <jrocks@uos.de>
2025-02-07 01:22:16 +0100

    Loaded CCAs are now also directly allowed

--- scripts/loadCCAs.sh
+++ scripts/loadCCAs.sh
@@ -7,40 +7,45 @@ BLD='\033[1;37m'
 RST='\033[0m'
 
 # Display the currently active congestion control algorithm
-current_cca=$(sysctl net.ipv4.tcp_congestion_control | awk -F' = ' '{print $2}')
+current_cca=$(sysctl -n net.ipv4.tcp_congestion_control)
 echo -e "Currently active congestion control algorithm: ${BLD}$current_cca${RST}"
 
-# Display currently loaded congestion control algorithms
-loaded_algos=$(sysctl -n net.ipv4.tcp_available_congestion_control | tr ' ' ', ')
-echo -e "Currently loaded congestion control algorithms: ${BLD}$loaded_algos${RST}"
+# Display available congestion control algorithms
+available_cca=$(sysctl -n net.ipv4.tcp_available_congestion_control)
+echo -e "Available congestion control algorithms: ${BLD}$available_cca${RST}"
+
+# Display currently allowed congestion control algorithms
+allowed_cca=$(sysctl -n net.ipv4.tcp_allowed_congestion_control)
+echo -e "Allowed congestion control algorithms: ${BLD}$allowed_cca${RST}"
 
 # Get list of available congestion control algorithms
 kernel_version=$(uname -r)
-available_algos=$(find /lib/modules/"$kernel_version"/kernel/net/ipv4/ -name 'tcp_*.ko*' -exec basename {} \; | sed 's/^tcp_//' | sed 's/\.ko.*$//' | tr '\n' ',' | sed 's/,$//')
+module_cca=$(find /lib/modules/"$kernel_version"/kernel/net/ipv4/ -name 'tcp_*.ko*' -exec basename {} \; | awk -F'[_|.]' '!/diag/ {print $2}' | paste -sd, -)
 
 # Display available algorithms
-if [ -n "$available_algos" ]; then
-    echo -e "Currently available congestion control algorithms: ${BLD}$available_algos${RST}"
+if [ -n "$module_cca" ]; then
+    echo -e "Found the module of the congestion control algorithms: ${BLD}$module_cca${RST}"
 else
-    echo -e "${RED}No available congestion control algorithms found.${RST}" >&2
+    echo -e "${RED}No available congestion control algorithms modules found.${RST}" >&2
     exit 1
 fi
 
-# Remove diag from the list of available algorithms
-available_algos=$(echo "$available_algos" | sed 's/diag,//')
-
 # Load the available congestion control algorithms
-IFS=',' read -r -a algos <<<"$available_algos"
-
-for algo in "${algos[@]}"; do
-    algo=$(echo "$algo" | xargs) # Trim whitespace
-    echo -n "Loading congestion control algorithm: $algo... "
-    if sudo modprobe "tcp_$algo"; then
+IFS=',' read -r -a algos <<<"$module_cca"
+for cca in "${algos[@]}"; do
+    cca=$(echo "$cca" | xargs) # Trim whitespace
+    echo -n "Loading congestion control algorithm: $cca... "
+    if sudo modprobe "tcp_$cca"; then
+        # Add the loaded algorithm to the allowed list by loading it
+        sudo sysctl -w net.ipv4.tcp_congestion_control="$cca" 1>/dev/null
         echo -e "${GRN}Success!${RST}"
     else
         echo -e "${RED}Failed to load!${RST}" >&2
     fi
 done
+
+# Reset the congestion control algorithm to the original value
+sudo sysctl -w net.ipv4.tcp_congestion_control="$current_cca" 1>/dev/null
 
 # Display instruction to temporarily change the congestion control algorithm
 echo -e "To temporarily change the congestion control algorithm run:\n${BLD}sudo sysctl -w net.ipv4.tcp_congestion_control=bbr${RST}"
