3a0416304892611bf1c39ccc3a17f288516ad919
Rasmus-123 <58940717+Rasmus-123@users.noreply.github.com>
2025-02-14 17:24:37 +0100

    check_whole_cc_folder.sh: Added BDP Filters and allow plotting

--- analysis/check_whole_cc_folder.sh
+++ analysis/check_whole_cc_folder.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-filters=("0-50-200-1 0-50-200-2 0-50-1000-1 0-50-1000-2 0-100-200-1 0-100-200-2 0-100-1000-1 0-100-1000-2")
+filters=("0-50-200-1 0-50-200-2 0-50-1000-1 0-50-1000-2 0-100-200-1 0-100-200-2 0-100-1000-1 0-100-1000-2 0-100-200-4 0-100-200-8 0-100-200-12 0-100-200-16")
 
 folder=$1
 echo "Checking folder $folder"
 
+plot=$2
+
 for filter in ${filters[@]}; do
     echo "Checking filter $filter"
-    python3 final/check_cc_folder.py $folder $filter n "$filter.res"
+    python3 final/check_cc_folder.py $folder $filter $2 "$filter.res"
 done
 
 if [ -z "$2" ]; then
